e50c3082a65669b6b3bb69fd5b41c9c8b5225f2c
WildBoarHenry123 <xiehh23@mail2.sysu.edu.cn>
2025-07-02 21:37:23 +0800

    feat: ✨ 新增层次分类 mong.py改为user.py 新增Token, Group, Speaker 添加了对应的基础操作函数
    ——谢慧豪

---
+++ app/models/Speaker.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+from uuid import uuid4
+from pymongo import MongoClient, ASCENDING
+from app.models.user import User
+'''
+class Speaker:
+
+    @staticmethod
+    def create()
+
+    @staticmethod   
+    def find()
+        
+    @staticmethod
+    def list()
+'''
